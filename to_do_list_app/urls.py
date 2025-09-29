05b9cceb770d6302bb1cdf1dc9375a98e47cca94
Raniya <raniyaak4@gmail.com>
2025-09-29 12:19:06 -0700

    29-09

--- to_do_list_app/urls.py
+++ to_do_list_app/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     path('add/', views.add,name='add'), 
     path('add_tasks/',views.add_tasks,name='add_tasks'),
     path('delete/<int:id>', views.delete,name='delete'), 
-    path('delete_tasks/<int:id>',views.delete_tasks,name='delete_tasks'),   
+    # path('delete_tasks/<int:id>',views.delete_tasks,name='delete_tasks'),   
     path('update_tasks/<int:id>',views.update_tasks,name='update_tasks'),
-    # path('edit/',views.edit,name='edit')
+
 ]
--- to_do_list_app/views.py
+++ to_do_list_app/views.py
@@ -2,37 +2,50 @@ from django.shortcuts import render
 from django.shortcuts import redirect, render,get_object_or_404
 from .models import Table
 from django.http import HttpResponse
+from datetime import date
 
 
+# def index(request):
+#     mytasks = Table.objects.all()
+
+#     return render(request, 'index.html', {"mytasks": mytasks, "today": today})
 # Create your views here.
 def index(request):
     mytasks = Table.objects.all().values()
-    return render(request,'index.html',{"mytasks": mytasks})
+    today = date.today()
+    return render(request,'index.html',{"mytasks": mytasks,"today": today})
 
 def add(request):
-    return render(request,'add.html')
+    today = date.today()
+    return render(request,'add.html',{"today": today})
+
 def add_tasks(request):
     task = request.POST['task']
     status = request.POST['status']
     status = request.POST.get('status','pending')
     due_date = request.POST.get("due_date")
+
     x=Table(task=task,status=status,due_date=due_date)
     x.save()
     return redirect('index')
 
-def update(request):
-    return render(request,'update.html')
+# def update(request):
+#     return render(request,'update.html')
 
 def update_tasks(request, id):
     tasks = Table.objects.get (id=id)
 
     if request.method == "POST":
+        new_due_date= request.POST.get("due_date")
+        if new_due_date and new_due_date < str(date.today()):
+            return HttpResponse("Error: Due date cannot be in the past!")
         tasks.task = request.POST.get("task")
         tasks.status = request.POST.get("status")
-        tasks.due_date = request.POST.get("due_date")
+        tasks.due_date = new_due_date
         tasks.save()
         return redirect("index")
-    return render(request, "update.html", {"tasks": tasks})
+    today = date.today()
+    return render(request, "update.html", {"tasks": tasks,"today":today})
 
 
 def delete(request, id):
@@ -42,10 +55,11 @@ def delete(request, id):
         return redirect("index")
     return render(request, "delete.html", {"mytasks": task}) 
 
-def delete_tasks(request,id):
-    tasks = Table.objects.get (id=id)
-    tasks.delete()
-    return redirect("delete")
+# def delete_tasks(request,id):
+#     tasks = Table.objects.get (id=id)
+#     tasks.delete()
+#     return redirect("delete")
+
 
 
 
